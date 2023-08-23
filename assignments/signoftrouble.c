509ef23a94746f18e587a03ccb2d504238ac4d23
JakeLille5253 <strictlybusinessjakelille@gmail.com>
2023-08-23 15:12:44 -0400

    Frequency array implementation, testing

--- assignments/signoftrouble.c
+++ assignments/signoftrouble.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 // max # of chars that can be read as input (including terminating null char)
-#define MAX_CHAR 100000
+#define MAX_CHAR 100002
+#define ASCII_SIZE 128
 
 // Rough Structure of program
 
@@ -17,8 +19,33 @@
     7. Upon ending program, return the counter value
 */
 
-int main(void){
+char *allocateString(int size);
 
+
+
+int main(void){
+    char *inputString = allocateString(MAX_CHAR);
+    char *freqArray1 = allocateString(ASCII_SIZE);
+    char *freqArray2 = allocateString(ASCII_SIZE);
+
+    printf("%s", "ENTER ORIGINAL MESSAGE: ");
+    fgets(inputString, MAX_CHAR, stdin);
+
+    for(int i = 0; i < strlen(inputString); i++){
+        freqArray1[(int)inputString[i]]++;
+    }
+ 
+    for(int i = 0; i < ASCII_SIZE; i++){
+        printf("%d, %d\n", i,freqArray1[i]);
+    }
+
+    //char *newString = malloc(MAX_CHAR*sizeof(char));
 
     return 0;
+}
+
+char *allocateString(int size){
+    char *string = malloc(size*sizeof(char));
+
+    return string; 
 }
